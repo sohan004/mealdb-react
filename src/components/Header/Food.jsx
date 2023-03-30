c9170b18529820c5f7ec594734cfdddd2e656f07
sohan004 <aryansohan02@gmail.com>
2023-03-30 15:04:04 +0600

    cart img add

--- src/components/Header/Food.jsx
+++ src/components/Header/Food.jsx
@@ -4,11 +4,11 @@ import { faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import './Food.css'
 
 const Food = (props) => {
-    const { id, name, price, quantity } = props.s
+    const { id, name,img, price, quantity } = props.s
     return (
         <li>
-            <div className='mb-3 gap-3 d-flex justify-content-between cart p-1 rounded'>
-                <p>{name.slice(0, 10)}</p>
+            <div className='mb-3 gap-3 d-flex justify-content-between align-items-center cart p-1 rounded'>
+                <img src={img} className="img-fluid " alt="" />
                 <p>{quantity}</p>
                 <p>${price * quantity}</p>
                 <div>
